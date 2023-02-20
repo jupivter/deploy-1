e18c145ddc449cb6e5a7e11c07f5200326d9f128
Marta Aramu <marta.aramu99@gmail.com>
2023-02-20 12:32:42 +0100

    Initial commit

---
+++ src/main/java/co/develhope/deploy1/BasicController.java
@@ -0,0 +1,19 @@
+package co.develhope.deploy1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/")
+    public String getDevName () {
+        return environment.getProperty("devName");
+    }
+
+}
